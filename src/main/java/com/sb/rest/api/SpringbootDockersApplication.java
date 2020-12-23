01590ad29ea74d0a0c96727b6d63394abc8e7426
Ravi Babu <kakani.addanki@gmail.com>
2020-12-23 12:37:04 +0530

    initial commit

---
+++ src/main/java/com/sb/rest/api/SpringbootDockersApplication.java
@@ -0,0 +1,12 @@
+package com.sb.rest.api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDockersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/rest/api/SpringbootDockersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
